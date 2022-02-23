98029bed9d0a71514c3aa06771dbffec5f3031d2
Angelo Cante <bob@alice.it>
2022-02-23 23:54:08 +0100

    lista da fare

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ new Vue ({
     el: '#app',
     data: {
         Titolo: 'Lista da Fare',
-        saluto: 'Ciao Sono Angelo',
-        giorno: new Date().getDate(),
-        mese: new Date().getMonth()+1,
-        anno: new Date ().getFullYear(),
-        ora: new Date().getHours(),
-        minuti: new Date().getMinutes(),
-        secondi: new Date().getSeconds(),
         inputVal: '',
         lista: [
             {}
